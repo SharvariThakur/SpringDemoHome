062408938c45f4c2141abfb8009ca8e0b8f15cfa
Sharvari <sharuthakur91@gmail.com>
2024-11-19 00:10:09 -0800

    remove the embebed tomcat

--- src/test/java/com/spring/demo/home/HomeApplicationTests.java
+++ src/test/java/com/spring/demo/home/HomeApplicationTests.java
@@ -1,9 +1,9 @@
 package com.spring.demo.home;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class HomeApplicationTests {
 
 	@Test
