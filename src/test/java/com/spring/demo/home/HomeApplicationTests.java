7e2d09832068220cbbfdca0dd9e61e8f053fca34
Sharvari <sharuthakur91@gmail.com>
2024-11-18 23:38:20 -0800

    initial commit

---
+++ src/test/java/com/spring/demo/home/HomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.demo.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
